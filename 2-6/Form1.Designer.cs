97477c0cc8ddb4ec926454135c54534403480a91
Александр Фокин <fokin-sany2012@yandex.ru>
2021-05-31 18:44:03 +0300

    Добавьте файлы проекта.

---
+++ 2-6/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace _2_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.trackBar4);
+            this.panel1.Controls.Add(this.trackBar3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.trackBar2);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1952, 1412);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.LargeChange = 20;
+            this.trackBar4.Location = new System.Drawing.Point(124, 397);
+            this.trackBar4.Minimum = 1;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(431, 80);
+            this.trackBar4.TabIndex = 5;
+            this.trackBar4.TickFrequency = 2;
+            this.trackBar4.Value = 1;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 1;
+            this.trackBar3.Location = new System.Drawing.Point(124, 297);
+            this.trackBar3.Minimum = 1;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(431, 80);
+            this.trackBar3.TabIndex = 4;
+            this.trackBar3.TickFrequency = 2;
+            this.trackBar3.Value = 1;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Black", 15.85714F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(1115, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(325, 63);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Задать цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Сплошная линия",
+            "Точки",
+            "Тире",
+            "Точка-тире",
+            "Точка-точка-тире"});
+            this.comboBox1.Location = new System.Drawing.Point(685, 79);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(325, 46);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Тип круга";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 20;
+            this.trackBar2.Location = new System.Drawing.Point(124, 194);
+            this.trackBar2.Maximum = 300;
+            this.trackBar2.Minimum = 10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(431, 80);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.TickFrequency = 2;
+            this.trackBar2.Value = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(124, 79);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(431, 80);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 2;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(231, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 38);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ширина границы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(231, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 38);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Размер круга";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(231, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 38);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Вытянуть по х";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(231, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 38);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Вытянуть по у";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1976, 1436);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 2-6/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_6
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        bool hold = false;
+        int x, y, r = 100,a = 3,b = 3;
+        Pen pen = new Pen(Color.Blue);
+        public Form1()
+        {
+            InitializeComponent();
+
+            g = Graphics.FromHwnd(panel1.Handle);
+
+            x = panel1.Width / 2;
+            y = panel1.Height / 2;
+            pen.Width = 10;
+            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            hold = true;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            hold = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            color.ShowDialog();
+            pen.Color = color.Color;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            pen.Width = trackBar1.Value;
+            panel1.Refresh();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            a = trackBar3.Value;
+            panel1.Refresh();
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            b = trackBar4.Value;
+            panel1.Refresh();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            r = trackBar2.Value;
+            panel1.Refresh();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idx = comboBox1.SelectedIndex;
+            switch (idx)
+            {
+                case 0:
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+                    break;
+                case 1:
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                    break;
+                case 2:
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                    break;
+                case 3:
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+                    break;
+                case 4:
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+                    break;
+                    
+            }
+            panel1.Refresh();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!hold) return;
+            x = e.Location.X;
+            y = e.Location.Y;
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {           
+            g.DrawEllipse(pen, x-r, y-r, r*a, r*b);
+        }
+    }
+}
